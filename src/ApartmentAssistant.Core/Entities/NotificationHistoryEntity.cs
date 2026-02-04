8e6f7cd3e3598a513d1d298d858376ded2212375
4view <akochergin.2002@gmail.com>
2026-02-04 15:05:19 +0500

    feat: Add entity that stores users notifications

---
+++ src/ApartmentAssistant.Core/Entities/NotificationHistoryEntity.cs
@@ -0,0 +1,14 @@
+using ApartmentAssistant.Core.Entities;
+
+public class NotificationHistoryEntity
+{
+    public Guid Id { get; set; }
+
+    public int UserId { get; set; }
+
+    public UserEntity? User { get; set; }
+
+    public DateTimeOffset NotificationDateTime { get; set; }
+
+    public DateTimeOffset CreatedAt { get; set; } = DateTimeOffset.Now;
+}
